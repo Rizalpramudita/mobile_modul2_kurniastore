3e659fe494e84f9bcac3b1fae80713ea50ee1b3d
Rizalpramudita <148871785+Rizalpramudita@users.noreply.github.com>
2025-10-13 20:51:13 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:kurniastore/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(KurniaStoreApp());
+    await tester.pumpWidget(const KurniaStoreApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
